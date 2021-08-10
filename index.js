a88f25526970a0b11434f20113cb5ecb318bfd7a
Debra Mella <debra.mella@libertymutual.com>
2021-08-10 18:46:40 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+// Load lodash using require() as assign it to a variable named lodash (easy to remember, right?)
+let lodash = require("lodash");
+
+let name = "brittany";
+console.log("Name: " + name);
+
+// call the upperFirst() function in lodash to convert the first letter of the string to uppercase
+name = lodash.upperFirst(name);
+console.log("Revised name: " + name);
+
+// call the upperFirst() function in lodash to convert the first letter of the string to uppercase
+name = lodash.lowerCase(name);
+console.log("Revised name: " + name);
+
+// call the upperFirst() function in lodash to convert the first letter of the string to uppercase
+name = lodash.upperCase(name);
+console.log("Revised name: " + name);
+
+// call the upperFirst() function in lodash to convert the first letter of the string to uppercase
+name = lodash.startCase(name);
+console.log("Revised name: " + name);
